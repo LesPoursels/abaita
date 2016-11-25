5ece4cdcf00eea4172a09553cab2d435959c84e7
fgiuba <federico.giuba@copangroup.com>
2016-11-25 12:22:09 +0100

    cazzi miei

--- abaita.py
+++ abaita.py
@@ -2,11 +2,11 @@
 
 
 import ftplib
-from datetime import datetime, date, timedelta
+from datetime import datetime, date
 from StringIO import StringIO
 
 
-your_code = 'code'
+your_code = 'caccia-il-codice-QUI'
 filename = 'btransaction.loc'
 ftp = ftplib.FTP("address")
 ftp.login("admin", "password")
@@ -16,7 +16,7 @@ r = StringIO()
 ftp.retrbinary('RETR btransaction.loc', r.write)
 rows = r.getvalue().split('\n')
 
-days = {}   # (year, month)
+months = {}   # (year, month)
 
 current_dt = None
 cday = None
@@ -28,27 +28,29 @@ for i, row in enumerate(rows):
 
         if dt is None or dt.date() != cday:
             cday = dt.date()
-            if (cday.year, cday.month) not in days:
-                days[(cday.year, cday.month)] = {}
-            if cday.day not in days[(cday.year, cday.month)]:
-                days[(cday.year, cday.month)][cday.day] = []
+            if (cday.year, cday.month) not in months:
+                months[(cday.year, cday.month)] = {}
+            if cday.day not in months[(cday.year, cday.month)]:
+                months[(cday.year, cday.month)][cday.day] = []
             print '\n[{}]'.format(str(cday))
 
         if ctime is None or ctime != dt.time():
             current_dt = dt
             ctime = dt.time()
-            days[(cday.year, cday.month)][cday.day].append(dt)
+            months[(cday.year, cday.month)][cday.day].append(dt)
             print ctime
         else:
             continue
 
-        if len(days[(cday.year, cday.month)][cday.day]) == 4:
-            times = days[(cday.year, cday.month)][cday.day]
+        if len(months[(cday.year, cday.month)][cday.day]) == 4:
+            times = months[(cday.year, cday.month)][cday.day]
             hours = (times[3] - times[2]) + (times[1] - times[0])
             print 'Ore sgobbate: {}'.format(hours)
 
-        if len(days[(cday.year, cday.month)][cday.day]) == 3 and cday == date.today():
-            times = days[(cday.year, cday.month)][cday.day]
+        if len(months[(cday.year, cday.month)][cday.day]) == 3 and cday == date.today():
+            times = months[(cday.year, cday.month)][cday.day]
             hours = (datetime.now() - times[2]) + (times[1] - times[0])
-            ape = "Dai che si fa l'ape!!!!!!" if cday.isoweekday() == 5 else ''
-            print 'Siamo a: {} ore...! {}'.format(hours, ape)
+            ape = "DAI CHE SI FA L'APE!!!!!!" if cday.isoweekday() == 5 else ''
+            print 'Siamo a: {} ore ... {}'.format(hours, ape)
+
+
