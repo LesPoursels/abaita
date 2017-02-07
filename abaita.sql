98624fa393301c001a54b99e20ca906c65e705ef
mippolito <michele.ippolito@copangroup.com>
2017-02-07 11:09:05 +0100

    Adds sql to create the abaita table

---
+++ abaita.sql
@@ -0,0 +1,8 @@
+CREATE TABLE abaita (
+    date date NOT NULL,
+    "time" time without time zone NOT NULL,
+    badge character varying NOT NULL,
+    uscita boolean NOT NULL,
+    raw character varying
+);
+
